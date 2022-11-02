439112d58819fef3efefdb6aa92af67aedb34b7c
LBlaze911 <113677370+LBlaze911@users.noreply.github.com>
2022-11-02 20:31:54 +0100

    1

--- Code Morse/Main.cpp
+++ Code Morse/Main.cpp
@@ -1,3 +1,15 @@
 #include<LettreMorse.hpp>
-#include<iostream.hpp>
+#include<Traducteur.hpp>
+//#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    system("pause");
+    return 0;
+}
+
 
